4d8fa83188a68c225ab5bfe0b96dfa26685cf362
David <david.cortez@facturedo.com>
2023-04-29 09:41:39 -0500

    Update Long Method exercise and Solution

--- solutions/long_method_refactored.py
+++ solutions/long_method_refactored.py
@@ -1,25 +1,26 @@
-def calculate_tax(income):
-    deductions = income * 0.1
-    taxable_income = income - deductions
-    tax = calculate_high_income_tax(taxable_income)
-    tax += calculate_medium_income_tax(taxable_income)
-    tax += calculate_low_income_tax(taxable_income)
-    return tax
+class TaxCalculator:
+    def calculate_tax(income):
+        deductions = income * 0.1
+        taxable_income = income - deductions
+        tax = calculate_high_income_tax(taxable_income)
+        tax += calculate_medium_income_tax(taxable_income)
+        tax += calculate_low_income_tax(taxable_income)
+        return tax
 
 
-def calculate_high_income_tax(taxable_income):
-    if taxable_income > 50000:
-        return 0.3 * (taxable_income - 50000)
-    return 0
+    def calculate_high_income_tax(taxable_income):
+        if taxable_income > 50000:
+            return 0.3 * (taxable_income - 50000)
+        return 0
 
 
-def calculate_medium_income_tax(taxable_income):
-    if taxable_income > 20000:
-        return 0.2 * min(taxable_income - 20000, 30000)
-    return 0
+    def calculate_medium_income_tax(taxable_income):
+        if taxable_income > 20000:
+            return 0.2 * min(taxable_income - 20000, 30000)
+        return 0
 
 
-def calculate_low_income_tax(taxable_income):
-    if taxable_income > 10000:
-        return 0.1 * min(taxable_income - 10000, 10000)
-    return 0
+    def calculate_low_income_tax(taxable_income):
+        if taxable_income > 10000:
+            return 0.1 * min(taxable_income - 10000, 10000)
+        return 0
